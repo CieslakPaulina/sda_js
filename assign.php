23dcd9f5abe00b75baf9b024ee0360a7ea409fa2
PaulinaCieslak <cieslak_paulina@wp.pl>
2020-02-09 13:05:27 +0100

    konkatanacja i wyswietlanie

---
+++ assign.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Imię psa lub kota
+ * @var string
+ */
+
+$name = 'Bobik';
+
+/**
+ * Ilość zajętych pokoi
+ * @var int
+ */
+
+$roomsCounter = 13;
+
+/**
+ * Nazwa zespołu występująca teraz na scenie
+ * @var string 
+ */
+
+$currectBandName = $name;
+
+/**
+ * wywołanie zmiennych
+ */
+echo "$name $roomsCounter $currectBandName";
